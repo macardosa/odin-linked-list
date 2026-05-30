f9377207ca76fa879a193afc34604c9c6618516f
Manuel Cardosa <macardosa@outlook.com>
2026-05-30 17:25:09 +0200

    Test it out

--- LinkedList.js
+++ LinkedList.js
@@ -1,4 +1,4 @@
-import { Node } from "./Node";
+import { Node } from "./Node.js";
 
 export function createLinkedList() {
     let head = null;
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-import { createLinkedList } from "./LinkedList";
+import { createLinkedList } from "./LinkedList.js";
+
+const list = createLinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString() === "( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null");
